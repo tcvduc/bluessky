c5edcbec8ceb1cb002d29200b5b68b96891ed411
Trinh Cao Van Duc <44729657+PanAngelosk@users.noreply.github.com>
2020-05-14 14:14:09 +0700

    env bug

--- weather_app/server/database/mongoose.js
+++ weather_app/server/database/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const connectionURL = "mongodb://127.0.0.1:27017/bluessky-user";
+const connectionURL = "mongodb://127.0.0.1:27017/bluessky";
 mongoose.connect(process.env.MONGODB_URL || connectionURL, {
   useNewUrlParser: true,
   useCreateIndex: true,
